608989764814c449c6f60c6897ea745f5c18a0e8
Devon <dstet02@gmail.com>
2021-12-06 12:23:10 -0500

    Interrupt startup if OpenSSL not installed

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QMessageBox>
+#include <QSslSocket>
 
 int main(int argc, char *argv[])
 {
 
     QApplication a(argc, argv);
+    if (!QSslSocket::supportsSsl())
+    {
+        QMessageBox diag;
+        diag.setText("OpenSSL not installed. Please install OpenSSL to use this program. The program will now close.");
+        diag.exec();
+
+        return 1;
+    }
     MainWindow w;
     w.show();
     return a.exec();
