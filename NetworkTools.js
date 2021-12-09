95a8c1afb300a680cea903e06c0d8f08a228a1e1
Devon <dstet02@gmail.com>
2021-12-09 01:16:20 -0500

    Start lerp function, not working atm

--- NetworkTools.js
+++ NetworkTools.js
@@ -19,11 +19,86 @@ function recommendationLoop(track)
         track = rec.tracks[0].id;
     }
 
+    let val = Math.floor(beegArray.length);
+
+    var featuresRes = [];
+
+    for (let k = 0; k < beegArray.length; k += 100)
+    {
+        if (k % 300 == 0)
+        {
+            token = getToken();
+        }
+
+        if (k % 100 == 0)
+        {
+            res.push(getAudioFeatures(token, beegArray[k].id));
+        }
+    }
+
+
+    var slopes = [];
+    for (let l = 0; l < featuresRes.length - 1; l++)
+    {
+        // accousticness / dX
+        slopes.push(
+                    {
+                        accousticness: (featuresRes[l + 1].acousticness - featuresRes[l].acousticness) / 100,
+                        danceability: (featuresRes[l + 1].danceability - featuresRes[l].danceability) / 100,
+                        energy: (featuresRes[l + 1].energy - featuresRes[l].energy) / 100,
+                        instrumentalness: (featuresRes[l + 1].instrumentalness - featuresRes[l].instrumentalness) / 100,
+                        liveness: (featuresRes[l + 1].liveness - featuresRes[l].liveness) / 100,
+                        speechiness: (featuresRes[l + 1].speechiness - featuresRes[l].speechiness) / 100,
+                        valence: (featuresRes[l + 1].valence - featuresRes[l].valence) / 100,
+                    });
+    }
+
+    var currentHundred = -100;
+    for (let m = 0; m < beegArray.length; m++)
+    {
+        if (m % 100 == 0)
+            currentSlope += 100;
+
+        let obj = {
+            // Lerp the values in the following format:
+            // beginning + ((i / 99) * (end - beginning))
+            //
+            // This is very messy I know I am sorry
+            accousticness: beegArray[0 + currentHundred].accousticness + (((m % 100) / 99) * (beegArray[99 + currentHundred].accousticness - beegArray[0 + currentHundred].accousticness)),
+            danceability: beegArray[0 + currentHundred].danceability + (((m % 100) / 99) * (beegArray[99 + currentHundred].danceability - beegArray[0 + currentHundred].danceability)),
+            energy: beegArray[0 + currentHundred].energy + (((m % 100) / 99) * (beegArray[99 + currentHundred].energy - beegArray[0 + currentHundred].energy)),
+            instrumentalness: beegArray[0 + currentHundred].instrumentalness + (((m % 100) / 99) * (beegArray[99 + currentHundred].instrumentalness - beegArray[0 + currentHundred].instrumentalness)),
+            liveness: beegArray[0 + currentHundred].liveness + (((m % 100) / 99) * (beegArray[99 + currentHundred].liveness - beegArray[0 + currentHundred].liveness)),
+            speechiness: beegArray[0 + currentHundred].speechiness + (((m % 100) / 99) * (beegArray[99 + currentHundred].speechiness - beegArray[0 + currentHundred].speechiness)),
+            valence: beegArray[0 + currentHundred].valence + (((m % 100) / 99) * (beegArray[99 + currentHundred].valence - beegArray[0 + currentHundred].valence)),
+        };
+
+        beegArray[m] = Object.assign(beegArray[m], obj);
+    }
+
+
     // Necessary to carry the values over to C++
     // VERY LARGE STRING
     let js = JSON.stringify(beegArray)
 
     return js;
+}
+
+function getAudioFeatures(token, track)
+{
+    var http = new XMLHttpRequest();
+    var url = `https://api.spotify.com/v1/audio-features/${track}`;
+    http.open("GET", url, false);
+
+    http.setRequestHeader("Content-Type", "application/json");
+    http.setRequestHeader("Authorization", `Bearer ${token}`);
+
+    http.send(null);
+
+    if (http.status === 200)
+    {
+        return http.responseText;
+    }
 }
 
 function getRecommendation(token, track)
